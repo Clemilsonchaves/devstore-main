5ef9c8462e757ef982bfa57603409bd21d876a5b
clemilsonchaves <clemilsonchaves.1992@gmail.com>
2025-05-24 04:32:43 -0300

    fix: update Cypress version and improve product search functionality in tests

--- cypress/e2e/add-product-to-cart.cy.ts
+++ cypress/e2e/add-product-to-cart.cy.ts
@@ -1,6 +1,6 @@
 describe('add product to cart', () => {
   beforeEach(() => {
-    cy.visit('/')
+    cy.visit('http://localhost:3000')
   })
 
   it('should be able to navigate to the product page and add it to the cart', () => {
@@ -25,9 +25,14 @@ describe('add product to cart', () => {
   it('should be able to search for a product and add it to the cart', () => {
     cy.searchByQuery('moletom')
 
-    cy.get('a[href^="/product"]').first().click()
+    // Aguarda o produto aparecer após a busca
+    cy.get('a[href^="/product"]', { timeout: 10000 })
+      .first()
+      .should('be.visible')
+      .click()
 
-    cy.location('pathname').should('include', '/product')
+    // Aguarda a navegação para a página do produto
+    cy.location('pathname', { timeout: 10000 }).should('include', '/product')
 
     cy.contains('Adicionar ao carrinho').click()
 
