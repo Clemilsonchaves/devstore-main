4da7ac50f9a3b103ae6aa0725cf5e9ef160a1f13
clemilsonchaves <clemilsonchaves.1992@gmail.com>
2025-05-25 05:01:05 -0300

    test: improve Cypress test for product search and cart addition functionality

--- cypress/e2e/search.cy.ts
+++ cypress/e2e/search.cy.ts
@@ -1,20 +1,23 @@
-describe('search products', () => {
-  it('should be able to search for products', () => {
+describe('Search and Add to Cart', () => {
+  it('should be able to search for a product and add it to the cart', () => {
     cy.searchByQuery('moletom')
 
-    cy.location('pathname').should('include', '/search')
-    cy.location('search').should('include', 'q=moletom')
+    // Aguarda o produto aparecer após a busca e estar estável
+    cy.get('a[href^="/product"]', { timeout: 10000 })
+      .first()
+      .should('be.visible')
+      .should('not.have.class', 'loading')
+      .click()
 
-    cy.get('a[href^="/product"]').should('exist')
-  })
+    // Aguarda a navegação para a página do produto
+    cy.location('pathname', { timeout: 10000 }).should('include', '/product')
 
-  it('should not be able to search page without a search query', () => {
-    cy.on('uncaught:exception', () => {
-      return false
-    })
+    // Garante que o botão está visível antes de clicar
+    cy.contains('Adicionar ao carrinho', { timeout: 10000 })
+      .should('be.visible')
+      .click()
 
-    cy.visit('/search')
-
-    cy.location('pathname').should('equal', '/')
+    // Verifica se o carrinho foi atualizado
+    cy.contains('Cart (1)').should('exist')
   })
 })
